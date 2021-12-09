5c726c7e0fad72e65b10a59e9dac1f6bbb97bbc5
* <godsfavour1975@gmail.com>
2021-12-10 00:55:39 +0100

    first push

---
+++ Index.js
@@ -0,0 +1,23 @@
+const addBtn = document.querySelector(".add");
+const display = document.querySelector(".display");
+const subtractBtn = document.querySelector(".subtract");
+let i = 0
+
+//event listener for the elements
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+
+
+// functions for the event listeners
+function add() {
+  i++;
+  display.innerHTML = i;
+}
+function subtract(){
+  i--;
+  display.innerHTML = i;
+  if (i == 0) {
+    alert("opps i can't move backwards again")
+    return false;
+  }
+}
